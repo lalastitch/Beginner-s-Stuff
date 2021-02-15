4323bd43b7921bb4068b33d08855f136f4b9da11
lalastitch <57478320+lalastitch@users.noreply.github.com>
2021-02-15 13:32:55 -0800

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,48 @@
+// First javascript code 
+// console.log('Hello World');
+
+// variables
+// cannot be a reserved keyword like if, let, var, etc.
+// let name = 'Mosh';
+// console.log(name);
+
+// const
+// const does not allow variable to change
+// will raise an error if you try to change it
+// const interest = 0.1;
+// console.log(interest);
+
+// object
+// like a dictionary
+//let person = {
+    //  name: 'Mosh',
+    //  age: 30
+// };
+// console.log(person)
+
+// dot notation
+// person.name = 'Jordan';
+
+// bracket notation
+// person['name'] = 'Pearl';
+
+// arrays
+// dynamic(can change at runtime)
+// let colors =['red','blue'];
+// colors[2] = 'green';
+// console.log(colors);
+// console.log(colors.length) returns the length of array
+
+// function
+// function greet(name){
+    // console.log('Hello' + name);
+// }
+// greet('Jordan');
+
+// function square(number){
+    // return number*number
+// }
+// let number = square(2);
+// console.log(number)
+// or
+// console.log(square(2));
